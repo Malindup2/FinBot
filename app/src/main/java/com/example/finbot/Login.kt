db02c8503a3b128eaec8a3f548c6d30dbb716ee9
Malindu Pabasara <139533700+Malindup2@users.noreply.github.com>
2025-04-03 23:29:40 +0530

    minor fix

---
+++ app/src/main/java/com/example/finbot/Login.kt
@@ -0,0 +1,21 @@
+package com.example.finbot
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import android.widget.Button
+import android.widget.TextView
+
+class Login : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        val loginButton = findViewById<Button>(R.id.login)
+        loginButton.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+}
