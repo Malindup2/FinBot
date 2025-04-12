63d9ee3c297626e00780cd866fa26dde36b56812
Malindu Pabasara <139533700+Malindup2@users.noreply.github.com>
2025-04-12 20:01:45 +0530

    configurations updated

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.example.finbot"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.finbot"
         minSdk = 29
-        targetSdk = 35
+        targetSdk = 33
         versionCode = 1
         versionName = "1.0"
 
